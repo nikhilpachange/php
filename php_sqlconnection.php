be7fb1ecdb236e2675cf664ce5b4744b21cb6493
Nikhil  Pachange <nikhil1pachange@gmail.com>
2024-11-10 18:50:35 +0530

    php_sqlconnection

---
+++ php_sqlconnection.php
@@ -0,0 +1,16 @@
+<?php
+
+$host="localhost";
+$userName="root";
+$password=null;
+$database="databasename";
+
+$conn= new mysqli($host,$userName,$password,$database);
+
+if($conn =>connection_error){
+    die("some error ".$conn=>connection_error);
+}
+
+echo "connection suscccesfull"
+
+?>
